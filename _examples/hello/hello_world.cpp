004e24a6198614628d28609024d8f85c56662ee3
Salvo Passaro <ilsalvopss@gmail.com>
2026-03-14 20:46:12 +0100

    example (hello_world): add sleep to the infinite loop; an empty loop causes modern compilers to emit ud2

--- _examples/hello/hello_world.cpp
+++ _examples/hello/hello_world.cpp
@@ -1,5 +1,6 @@
 
 #include "mxgui/entry.h"
+#include <thread>
 #include "mxgui/display.h"
 
 using namespace mxgui;
@@ -10,5 +11,6 @@ ENTRY()
         DrawingContext dc(DisplayManager::instance().getDisplay());
         dc.write(Point(0,1),"Hello World!");
     }
-    for(;;) ;
+    for(;;)
+        std::this_thread::sleep_for(std::chrono::seconds(1));  // otherwise compilers assume unreachable and emit ud2
 }
