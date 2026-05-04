88f228eb97073e94cec4af77e9aa90206983ab79
Salvo Passaro <ilsalvopss@gmail.com>
2026-05-04 20:38:14 +0200

    examples, buttonTestFixedFor3220g: update example with new WindowManager's APIs (keeping whatever is going on with (0,319))

--- _examples/buttonTest/buttonTestFixedFor3220g.cpp
+++ _examples/buttonTest/buttonTestFixedFor3220g.cpp
@@ -1,80 +1,83 @@
 #include <entry.h>
-#include <memory>
 #include <level2/widgets/checkbox.h>
 #include <level2/widgets/button.h>
 #include <level2/widgets/radio_button.h>
 #include <level2/widgets/label.h>
-#include <iostream>
-#include "fps_counter.h"
+
 using namespace std;
 using namespace mxgui;
 using namespace mxgui::widgets;
 
 ENTRY()
 {
-    
-    int counter=0;
-    RadioGroup rg=RadioGroup();
-    RadioGroup rg2=RadioGroup();
-    shared_ptr<Window> w= make_shared<Window>();
-    Button b1=Button(w.get(),Rect(Point(10,10),Point(70,40)),"Button 1");
-    Button b1=Button(w.get(),Rect(Point(10,10),Point(70,40)),"Button 1");
-    
+    int counter = 0;
+    auto rg = widgets::RadioGroup();
+    auto rg2 = widgets::RadioGroup();
 
-    Label l1=Label(w.get(),Point(110,10),5,20,"0");
+    // Create a WindowPreference object to be moved into createWindow()
+    WindowPreferences prefs;
+    prefs.height = 315;
+    prefs.width = 235;
+    prefs.background = darkGrey;
 
-    CheckBox c1=CheckBox(w.get(),Point(10,60),15,"Check 1");
-    Label l2=Label(w.get(),Point(110,60),80,20,"false");
-    CheckBox c2=CheckBox(w.get(),Point(10,80),15,"Check 2",true);
-    Label l5=Label(w.get(),Point(110,80),80,20,"true");
+    // Ask the WindowManager for a Window @ (5,5) and get a Window::Handle
+    auto w = WindowManager::instance().createWindow({5, 5}, std::move(prefs));
 
-    c1.setCallback([&l2,&c1](){
-        string s="false";
-        if(c1.isChecked())
-            s="true";
+    // Since in this demo w is the only window, let's terminate when it is closed!
+    w.registerOnClose([]() { std::exit(0); });
+
+    // Create a bunch of widgets inside the Window w:
+    auto& b1 = w.make<widgets::Button>(Rect(Point(10, 10), Point(70, 40)), "Button 1");
+    auto& l1 = w.make<widgets::Label>(Point(110, 10), 5, 20, "0");
+
+    auto& c1 = w.make<widgets::CheckBox>(Point(10, 60), 15, "Check 1");
+    auto& l2 = w.make<widgets::Label>(Point(110, 60), 80, 20, "false");
+    auto& c2 = w.make<widgets::CheckBox>(Point(10, 80), 15, "Check 2", true);
+    auto& l5 = w.make<widgets::Label>(Point(110, 80), 80, 20, "true");
+
+    // Use references to Widgets inside a callback of a Widget belonging to the same Window!
+    c1.setCallback([&l2,&c1]() {
+        string s = "false";
+        if (c1.isChecked())
+            s = "true";
         l2.setText(s);
     });
 
-    c2.setCallback([&l5,&c2](){
-        string s="false";
-        if(c2.isChecked())
-            s="true";
+    c2.setCallback([&l5,&c2]() {
+        string s = "false";
+        if (c2.isChecked())
+            s = "true";
         l5.setText(s);
     });
 
-    RadioButton r00=RadioButton(w.get(),&rg2,Point(10,110),15,"Mario");
-    RadioButton r01=RadioButton(w.get(),&rg2,Point(10,130),15,"Luigi");
-    Label l4=Label(w.get(),Point(110,110),70,20,"None");
+    // Create even more widgets
+    auto& r00 = w.make<widgets::RadioButton>(&rg2, Point(10, 110), 15, "Mario");
+    auto& r01 = w.make<widgets::RadioButton>(&rg2, Point(10, 130), 15, "Luigi");
+    auto& l4 = w.make<widgets::Label>(Point(110, 110), 70, 20, "None");
     auto cb2 = [&rg2,&l4]() { l4.setText(rg2.getChecked()->getLabel()); };
 
     r00.setCallback(cb2);
     r01.setCallback(cb2);
-    
-   
-    b1.setCallback([&l1,&counter](){
-        counter=counter+1;
-        counter=counter%10;
-        l1.setText(to_string(counter));
-    });
 
-    RadioButton r1=RadioButton(w.get(),&rg,Point(10,160),15,"Radio 1");
-    RadioButton r2=RadioButton(w.get(),&rg,Point(10,200),15,"Radio 2");
-    RadioButton r3=RadioButton(w.get(),&rg,Point(10,240),15,"Radio 3");
-    
-    Label l3=Label(w.get(),Point(110,160),70,20,"Radio 1");
+    auto& r1 = w.make<widgets::RadioButton>(&rg, Point(10, 160), 15, "Radio 1");
+    auto& r2 = w.make<widgets::RadioButton>(&rg, Point(10, 200), 15, "Radio 2");
+    auto& r3 = w.make<widgets::RadioButton>(&rg, Point(10, 240), 15, "Radio 3");
+
+    auto& l3 = w.make<widgets::Label>(Point(110, 160), 70, 20, "Radio 1");
     auto cb = [&rg,&l3]() { l3.setText(rg.getChecked()->getLabel()); };
 
     r1.setCallback(cb);
     r2.setCallback(cb);
     r3.setCallback(cb);
     rg.setChecked(&r1);
-    
-
-    
-
+    b1.setCallback([&l1,&counter]() {
+        counter = ++counter % 10;
+        l1.setText(to_string(counter));
+    });
     
     Point pOld=Point(-1,-1);
-    InputHandler::instance().registerEventCallback([w,&pOld](){
+    // At this point the WindowManager has already registered its internal callback. Let's overwrite it
+    InputHandler::instance().registerEventCallback([&pOld](){
         Event e = InputHandler::instance().popEvent();
         //cout<<"Event: "<<e.getEvent()<<endl;
         Point p = e.getPoint();
@@ -82,10 +85,11 @@ ENTRY()
             p=pOld;
         e=Event(e.getEvent(),p);
         pOld=p;
-        w.get()->postEvent(e);
+        WindowManager::instance().pushMessage(WindowManager::WMMessage(std::move(e)));
     });
-    WindowManager::instance().start(w);
-    w.get()->eventLoop();
 
-    return 0;
+    // Start the WindowManager's event loop.
+    WindowManager::instance().loop();
+
+    // Code will never reach here!
 }
