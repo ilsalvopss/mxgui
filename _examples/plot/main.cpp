55668a6537e2d878f87f99c794146cd5293d9c10
Salvo Passaro <ilsalvopss@gmail.com>
2026-05-05 14:14:03 +0200

    examples/plot: adapt example to new SimplePlot and WindowManager API

--- _examples/plot/main.cpp
+++ _examples/plot/main.cpp
@@ -1,34 +1,57 @@
 
 #include <entry.h>
-#include <display.h>
 #include <level2/widgets/simple_plot.h>
 #include <unistd.h>
 #include <cmath>
+#include <thread>
+
+#include "level2/application.h"
+#include "level2/widgets/button.h"
 
 using namespace std;
 using namespace mxgui;
 
 ENTRY()
 {
+    // Create a WindowPreference object to be moved into createWindow()
+    WindowPreferences prefs;
+    prefs.height = 230;
+    prefs.width = 235;
+    prefs.background = darkGrey;
+
+    // Ask the WindowManager for a Window @ (5,5) and get a Window::Handle
+    auto w = WindowManager::instance().createWindow({5, 5}, std::move(prefs));
+    auto& exit_btn = w.make<widgets::Button>(Rect{{7,205}, {230,222}}, "Close, please");
+    exit_btn.setCallback([&w] { w.close(); });
+
     int i=0;
     vector<float> data1; 
     vector<float> data2;
     
-    vector<Dataset> dataset;
-    dataset.push_back(Dataset(data1,red));
-    dataset.push_back(Dataset(data2,green));
-    
-    Display& display=DisplayManager::instance().getDisplay();
-    SimplePlot plot1(Point(0,0),Point(239,200));
-    
-    for(;;i+=2)
-    {
-        {
-            DrawingContext dc(display);
-            plot1.draw(dc,dataset);
-        }
-        data1.push_back(20*(1+0.05*i)*sin(0.1*i));
-        data2.push_back(20*(1+0.05*i));
-        usleep(100000);
-    }
+    vector<widgets::SimplePlot::Dataset> dataset;
+    dataset.push_back(widgets::SimplePlot::Dataset(data1,red));
+    dataset.push_back(widgets::SimplePlot::Dataset(data2,green));
+
+    auto& plotter = w.make<widgets::SimplePlot>(Rect{{0,0}, {235,200}});
+    std::atomic<bool> go = true;
+    w.registerOnClose([&go]() { go = false; });
+
+    std::thread t([&] {
+        w.whileAlive([&](Window&) {
+            for(;;i+=2)
+            {
+                if (!go)
+                    break;
+
+                plotter.plot(dataset);
+
+                data1.push_back(20*(1+0.05*i)*sin(0.1*i));
+                data2.push_back(20*(1+0.05*i));
+                usleep(100000);
+            }
+        });
+    });
+    t.detach();
+
+    WindowManager::instance().loop();
 }
